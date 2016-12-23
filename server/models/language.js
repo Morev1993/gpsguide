1d0913943cdac8d2749c4111678f9415d268015f
igor <morev.i@mail.ru>
2016-12-23 23:15:48 +1000

    fixes again

---
+++ server/models/language.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+//================================
+// User Schema
+//================================
+var LanguageSchema = new Schema({
+    accountId: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    code: {
+        type: String,
+        required: true
+    },
+    locale: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: Boolean,
+        default: false
+    },
+    orderBy: {
+        type: Number,
+        default: 0
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Language', LanguageSchema);
