7e38fdb9226e9dec58105609e78cf6be543e9833
igor <morev.i@mail.ru>
2017-01-12 20:57:59 +1000

    minor auth rewrite

--- server/controllers/device/deviceAuth.js
+++ server/controllers/device/deviceAuth.js
@@ -14,6 +14,7 @@ function generateToken(data) {
 //= =======================================
 // Login Route
 //= =======================================
+
 exports.login = function(req, res, next) {
     var authCode = req.body.code;
     var model = req.body.model;
@@ -23,14 +24,7 @@ exports.login = function(req, res, next) {
 
     var deviceIdOut = false;
 
-    // Return error if no email provided
-    /*if (!model) {
-        return res.status(422).send({
-            error: 'Model is empty.'
-        });
-    }*/
-
-    // Return error if no password provided
+    // return error if no password provided
     if (!authCode) {
         return res.status(422).send({
             error: 'You must enter an auth code.'
@@ -50,49 +44,41 @@ exports.login = function(req, res, next) {
     }
 
     Device.findOne({
-        authCode,
-        deviceId
+        authCode
     }).then(device => {
-    	if (device) {
-    		console.log('device id and auth code is right');
-    		var deviceInfo = setDeviceInfo(device);
-
-    		res.status(201).json({
-	            success: true,
-	            token: `JWT ${generateToken(deviceInfo)}`,
-	            data: device
-	        });
-	        
-	        return Promise.reject();
-    	} else {
-    		// device with device_id is gone and we should find by authcode
-    		return Device.findOne({ authCode });
-    	}
-    }).then(device => {
-    	if (!device) {
-			res.status(422).send({
+        if (!device) {
+            res.status(422).send({
 	            error: 'Wrong auth code.'
 	        });
+            return Promise.reject();
+        }
 
-	        return Promise.reject();
-    	} else if (device) {
-    		if (!device.deviceId) {
-				device.device = model || device.device;
-		        device.version = version || device.version;
-		        device.status = true;
-		        device.deviceId = deviceId;
+        if (!device.deviceId) {
+            device.device = model || device.device;
+            device.version = version || device.version;
+            device.status = true;
+            device.deviceId = deviceId;
 
-		        return device.save();
-    		} else {
-				res.status(422).send({
-		            error: 'Wrong device id.'
-		        });
+            return device.save();
+        } else if (device.deviceId === deviceId) {
+            var deviceInfo = setDeviceInfo(device);
 
-		        return Promise.reject();
-    		}
-    	}
-	}).then(device => {
-		var deviceInfo = setDeviceInfo(device);
+            res.status(201).json({
+                success: true,
+                token: `JWT ${generateToken(deviceInfo)}`,
+                data: device
+            });
+
+            return Promise.reject();
+        } else {
+            res.status(422).send({
+                error: 'Wrong device id.'
+            });
+
+            return Promise.reject();
+        }
+    }).then(device => {
+        var deviceInfo = setDeviceInfo(device);
 
         res.status(201).json({
             success: true,
